bb9cfba2c05d4375dc8afa29dd1d10ce6f9c6131
Surendra Maurya <surendra75maurya@gmail.com>
2022-03-30 12:24:10 +0530

    Added usecontext file

---
+++ my-app/src/Test1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test1 = () => {
+    return ( 
+
+        <>
+        <h1>Hii my</h1>
+        </>
+     );
+}
+ 
+export default Test1;
---
+++ my-app/src/Test2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test2 = () => {
+    
+    return (
+        <>
+        
+        <h1>This is </h1>
+        </>
+      );
+}
+ 
+export default Test2;
